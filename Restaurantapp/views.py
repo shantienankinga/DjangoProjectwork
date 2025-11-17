071fa64c8bb3788e4779cce7e809087939dd247a
nankingashanitah64@gmail.com <nankingashanita64@gmail.com>
2025-11-17 17:14:04 +0300

    Fix popup notification

--- Restaurantapp/views.py
+++ Restaurantapp/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from django.contrib import messages 
 # Create your views here.
 def category_veiw(request):
     return render (request, 'category.html')
@@ -29,31 +30,30 @@ def waiter_veiw(request):
 
 @ login_required
 def add_waiter_veiw (request):
-    message = ''
     waiters = Waiter.objects.all()
     if request.method =="POST":
         waiter_form = WaiterForm(request.POST)
         if waiter_form.is_valid():
             waiter_form.save()
-            message="waiter added successfully"
+            messages.success(request,"waiter-added sucessfully")
+        else:
+            messages.error(request,"form is invalid")
     else:
         waiter_form = WaiterForm()
     context = {
         'form' : waiter_form,
-        'msg' : message,
         'waiters' : waiters,
     }
     return render(request ,"add_waiter.html" , context)
 
 @ login_required
 def edit_waiter_veiw(request, waiter_id):
-    message=''
     waiter = Waiter.objects.get(id = waiter_id)
     if request.method == "POST":
         Waiter_form  = WaiterForm(request.POST, instance=waiter)
         if Waiter_form.is_valid():
             Waiter_form.save()
-            message="Changes saved sucessfully"
+            messages.success(request,"changes saved")
             return redirect(add_waiter_veiw)
         else:
             message="form has invalid data"
@@ -63,7 +63,6 @@ def edit_waiter_veiw(request, waiter_id):
     context ={
         'form' : Waiter_form,
         'waiter' : waiter, 
-        'message' : message,
     }
     return render (request ,'edit_waiter.html',context)
 
@@ -71,24 +70,25 @@ def edit_waiter_veiw(request, waiter_id):
 def delete_waiter_veiw(request, waiter_id) :
     waiter = Waiter.objects.get(id=waiter_id)
     waiter.delete()
+    messages.success(request,"deleted successfully")
     return redirect (add_waiter_veiw)
 
 
 @ login_required
 def add_category_veiw(request):
-    message = ''
     categorys = Category.objects.all()
     if request.method == "POST":
         category_form = CategoryForm(request.POST)
         if category_form.is_valid():
             category_form.save()
-            message = "Category Added Successfully"
+            messages.success(request,"category-added sucessfully")
+        else:
+            messages.error(request,"form is invalid")
     else:
         category_form = CategoryForm()
         
     context = {
         'form' : category_form,
-        'msg' : message,
         'categorys' : categorys,
     }
     return render(request ,"add_category.html", context)
@@ -101,16 +101,15 @@ def edit_category_veiw(request,category_id):
         category_form = CategoryForm(request.POST, instance=category)
         if category_form.is_valid():
             category_form.save()
-            message = "Changes Saved "
+            messages.success(request,"changes saved")
             return redirect(add_category_veiw)
         else :
-            message = "Form has invalid data"
+            messages.error(request , "form has invalid data")
     else:
         category_form = CategoryForm(instance=category)
         context = {
             'form' : category_form,
             'category' : category,
-            'message' : message,
 
         }
         return render (request,'edit_category.html',context)
@@ -120,23 +119,24 @@ def edit_category_veiw(request,category_id):
 def delete_category_veiw(request, category_id):
         category = Category.objects.get(id=category_id)
         category.delete()
+        messages.success(request,"deleted successfully")
         return redirect (add_category_veiw)
 
 @ login_required
 def add_menu_veiw (request):
-    message = ''
     menus = Menu.objects.all()
     if request.method =="POST":
         menu_form = MenuForm(request.POST)
         if menu_form.is_valid():
             menu_form.save()
-            message="menu added successfully"
+            messages.success(request,"menu added")
+        else:
+            messages.error(request,"form is invalid")
     else:
         menu_form = MenuForm()
         
     context = {
         'form' : menu_form,
-        'msg' : message,
         'menus' : menus,
     }
     return render(request ,"add_menu.html" , context)
@@ -149,10 +149,10 @@ def edit_menu_veiw(request, menu_id):
         menu_form  = MenuForm(request.POST, instance=menu)
         if menu_form.is_valid():
             menu_form.save()
-            message="Changes saved sucessfully"
+            messages.success(request,"Changes saved")
             return redirect(add_menu_veiw)
         else:
-            message="form has invalid data"
+            messages.error(request,"form is invalid")
     else:
         menu_form = MenuForm(instance=menu)
     context ={
@@ -166,91 +166,95 @@ def edit_menu_veiw(request, menu_id):
 def delete_menu_veiw(request, menu_id) :
     menu = Menu.objects.get(id=menu_id)
     menu.delete()
+    messages.success(request,"deleted successfully")
     return redirect (add_menu_veiw)
 
 @ login_required
 def add_menu_item_veiw (request):
-    message = ''
+
     menuitems = Menu_item.objects.all()
     if request.method =="POST":
         menu_item_form = MenuitemForm(request.POST)
         if menu_item_form.is_valid():
             menu_item_form.save()
-            message="menuitem added successfully"
+            messages.success(request,"menu item added")
+        else:
+            messages.error(request,"form is invalid")
     else:
         menu_item_form = MenuitemForm()
         
     context = {
         'form' : menu_item_form,
-        'msg' : message,
+    
         'menuitems' : menuitems,
     }
     return render(request ,"add_menu_item.html" , context)
 
 @ login_required
 def edit_menu_item_veiw(request, menuitem_id):
-    message=''
+    
     menuitem = Menu_item.objects.get(id = menuitem_id)
     if request.method == "POST":
         menu_item_form  = MenuitemForm(request.POST, instance=menuitem)
         if menu_item_form.is_valid():
             menu_item_form.save()
-            message="Changes saved sucessfully"
+            messages.success(request,"chamges saved")
             return redirect(add_menu_item_veiw)
         else:
-            message="form has invalid data"
+            messages.error(request,"form has invalid data")
     else:
         menu_item_form = MenuitemForm(instance=menuitem)
     context ={
         'form' : menu_item_form,
         'menuitem' : menuitem,
-        'message' : message,
     }
     return render (request ,'edit_menu_item.html',context)
 
 def delete_menu_item_veiw(request, menuitem_id) :
     menuitem = Menu_item.objects.get(id=menuitem_id)
     menuitem.delete()
+    messages.success(request,"deleted successfully")
     return redirect (add_menu_item_veiw)
 
 
 @ login_required
 def add_food_item_veiw (request):
-    message = ''
     fooditems = Food_item.objects.all()
     if request.method =="POST":
         food_item_form = FooditemForm(request.POST)
         if food_item_form.is_valid():
             food_item_form.save()
-            message="fooditem added successfully"
+            messages.success(request,"changes saved")
+        else:
+            messages.error(request,"form is invalid")
+        
     else:
         food_item_form = FooditemForm()
         
     context = {
         'form' : food_item_form,
-        'msg' : message,
         'fooditems' : fooditems,
     }
     return render(request ,"add_food_item.html" , context)
 
 @ login_required
 def edit_food_item_veiw(request, fooditem_id):
-    message=''
     fooditem = Food_item.objects.get(id = fooditem_id)
     if request.method == "POST":
         food_item_form  = FooditemForm(request.POST, instance=fooditem)
         if food_item_form.is_valid():
             food_item_form.save()
-            message="Changes saved sucessfully"
+            messages.success(request,"Changes saved sucessfully")
+        
             return redirect(add_food_item_veiw)
         else:
-            message="form has invalid data"
+            messages.error(request,"form has invalid data")
     else:
         food_item_form = FooditemForm(instance=fooditem)
     context ={
         'form' : food_item_form,
         'fooditem' : fooditem,
-        'message' : message,
+    
     }
     return render (request ,'edit_food_item.html',context)
 
@@ -258,24 +262,27 @@ def edit_food_item_veiw(request, fooditem_id):
 def delete_food_item_veiw(request, fooditem_id) :
     fooditem = Food_item.objects.get(id=fooditem_id)
     fooditem.delete()
+    messages.success(request,"deleted successfully")
     return redirect (add_food_item_veiw)
 
 
 @ login_required
 def add_order_item_veiw (request):
-    message = ''
+
     orderitems = Order_item.objects.all()
     if request.method =="POST":
         order_item_form = OrderitemForm(request.POST)
         if order_item_form.is_valid():
             order_item_form.save()
-            message="orderitem added successfully"
+            messages.success(request,"orderitem added successfully")
+        else:
+            messages.error(request,"form is invalid")
     else:
         order_item_form = OrderitemForm()
         
     context = {
         'form' : order_item_form,
-        'msg' : message,
+    
         'orderitems' : orderitems,
     }
     return render(request ,"add_order_item.html" , context)
@@ -288,10 +295,10 @@ def edit_order_item_veiw(request, orderitem_id):
         order_item_form  = OrderitemForm(request.POST, instance=orderitem)
         if order_item_form.is_valid():
             order_item_form.save()
-            message="Changes saved sucessfully"
+            messages.success(request,"Changes saved sucessfully")
             return redirect(add_order_item_veiw)
         else:
-            message="form has invalid data"
+            messages.error(request,"form is invalid")
     else:
         order_item_form = OrderitemForm(instance=orderitem)
     context ={
@@ -305,45 +312,46 @@ def edit_order_item_veiw(request, orderitem_id):
 def delete_order_item_veiw(request, orderitem_id) :
     orderitem = Order_item.objects.get(id=orderitem_id)
     orderitem.delete()
+    messages.success(request,"deleted successfully")
     return redirect (add_order_item_veiw)
 
 @ login_required
 def add_customer_order_veiw (request):
-    message = ''
+    
     customerorders = Customer_order.objects.all()
     if request.method =="POST":
         customer_order_form = CustomerorderForm(request.POST)
         if customer_order_form.is_valid():
             customer_order_form.save()
-            message="customer-order added successfully"
+            messages.success(request,"customer-order added successfully")
+        else:
+            messages.error(request,"form is invalid")
     else:
         customer_order_form = CustomerorderForm()
         
     context = {
         'form' : customer_order_form,
-        'msg' : message,
         'customerorders' : customerorders,
     }
     return render(request ,"add_customer_order.html" , context)
 
 @ login_required
 def edit_customer_order_veiw(request, customerorder_id):
-    message=''
+
     customerorder = Customer_order.objects.get(id = customerorder_id)
     if request.method == "POST":
         customer_order_form  = CustomerorderForm(request.POST, instance=customerorder)
         if customer_order_form.is_valid():
             customer_order_form.save()
-            message="Changes saved sucessfully"
+            messages.success(request,"Changes saved sucessfully")
             return redirect(add_customer_order_veiw)
         else:
-            message="form has invalid data"
+            messages.error(request,"form is invalid")
     else:
         customer_order_form = CustomerorderForm(instance=customerorder)
     context ={
         'form' : customer_order_form,
         'customerorder' : customerorder,
-        'message' : message,
     }
     return render (request ,'edit_customer_order.html',context)
 
@@ -351,25 +359,26 @@ def edit_customer_order_veiw(request, customerorder_id):
 def delete_customer_order_veiw(request, customerorder_id) :
     customerorder = Customer_order.objects.get(id=customerorder_id)
     customerorder.delete()
+    messages.success(request,"deleted successfully")
     return redirect (add_customer_order_veiw)
     
 @ login_required
 def add_restuarant_table_veiw (request):
-    message = ''
+
     restuaranttables = Restaurant_table.objects.all()
     if request.method =="POST":
         restuarant_table_form = RestuaranttableForm(request.POST)
         if restuarant_table_form.is_valid():
             restuarant_table_form.save()
-            message="restuarant-table added successfully"
+            messages.success(request,"restuarant-table added successfully")
         else:
-            message = "error saving table"
+            messages.error(request,"form is invalid")
     else:
         restuarant_table_form = RestuaranttableForm()
         
     context = {
         'form' : restuarant_table_form,
-        'msg' : message,
+        
         'restuaranttables' : restuaranttables,
     }
     return render(request ,"add_restuarant_table.html" , context)
@@ -382,10 +391,10 @@ def edit_restuarant_table_veiw(request, restuaranttable_id):
         restuarant_table_form  = RestuaranttableForm(request.POST, instance=restuaranttable)
         if restuarant_table_form.is_valid():
             restuarant_table_form.save()
-            message="Changes saved sucessfully"
+            messages.success(request ,"Changes saved sucessfully")
             return redirect(add_restuarant_table_veiw)
         else:
-            message="form has invalid data"
+            messages.error(request,"form is invalid")
     else:
         restuarant_table_form = RestuaranttableForm(instance=restuaranttable)
     context ={
@@ -398,6 +407,7 @@ def edit_restuarant_table_veiw(request, restuaranttable_id):
 def delete_restuarant_table_veiw(request, restuaranttable_id) :
     restuaranttable = Restaurant_table.objects.get(id=restuaranttable_id)
     restuaranttable.delete()
+    messages.success(request,"deleted successfully")
     return redirect (add_restuarant_table_veiw)
 
 
@@ -408,9 +418,9 @@ def sign_up_view(request):
         
         if sign_up_form.is_valid():
             sign_up_form.save()
-            message = 'User created successfully'
+            messages.success(request,"user created")
         else:
-            message = 'Access Denayed'
+            messages.error(request,"access denayed")
     else:
         sign_up_form = UserCreationForm()
     context = {
